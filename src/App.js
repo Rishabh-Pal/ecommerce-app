e20548e6e428c6fb48cc6a1b344a6a8f73a19ac9
Rishabh@iv5394 <boletorishabh1999@gmail.com>
2023-07-25 19:31:33 +0530

    all job is done

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer'
+import Home from './components/Home'
+import About from './components/About'
+import Product from './components/Product'
+import Contact from './components/Contact'
+import { Route, Routes} from 'react-router-dom';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Header/>
+      
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/products" element={<Product/>}/>
+          <Route path="/products/:id" element={<ProductDetail/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/checkout" element={<Checkout/>} />
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      
+      <Footer/>
+    </>
   );
 }
 
